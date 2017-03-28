9834aeaaf8398ba7e445367246bfed6f842e1961
Nate Ricklin <nate@rickl.in>
2017-03-28 16:20:59 -0600

    Create get_s3_creds.py

---
+++ get_s3_creds.py
@@ -0,0 +1,9 @@
+from gbdxtools import Interface
+gbdx = Interface()
+
+creds = gbdx.s3.info
+# print out the json results
+print "[DEFAULT]"
+print "aws_secret_access_key=" + creds['S3_secret_key']
+print "aws_access_key_id=" + creds['S3_access_key']
+print "aws_session_token=" + creds['S3_session_token']
